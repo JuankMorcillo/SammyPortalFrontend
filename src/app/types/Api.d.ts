80c4ee60e7d54380b5d77dcb08394152b20dc23f
@juankmorcillo2 <juankmorcillo2@gmail.com>
2026-03-07 18:39:44 -0500

    feature Se crean tipos para autenticacion y envio de datos al api

---
+++ src/app/types/Api.d.ts
@@ -0,0 +1,16 @@
+declare global {
+    type OptionsProps = {
+        url: string,        
+        method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH',
+        data?: any,
+        body?: any,
+        headers?: any,
+        params?: any
+    }
+    type FetchPayload = {
+        token: string,
+        params: Params
+    }
+}
+
+export { }
---
+++ src/app/types/next-auth.d.ts
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth"
+
+declare module "next-auth" {
+
+    interface Session {
+        user: User
+    }
+    
+    interface User {
+        token: string,       
+    }
+
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        token?: string,        
+    }
+}
