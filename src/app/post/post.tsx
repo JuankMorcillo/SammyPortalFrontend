bd6147e28a6f53e244d865b35efd4315cded5592
@juankmorcillo2 <juankmorcillo2@gmail.com>
2026-03-07 20:33:22 -0500

    feature Se leen post desde el api. Refactor se coloca opcional una propiedad

--- src/app/post/post.tsx
+++ src/app/post/post.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+'use client';
+
+import React, { useEffect, useState } from 'react'
 import { PostProps } from '../types/post'
 import PostsCards from '../components/posts_cards'
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../store';
+import { useSession } from 'next-auth/react';
+import { fetchPosts } from '../store/slices/postSlice';
 
 const PostExpample: PostProps[] = [{
     id: 1,
@@ -20,11 +26,28 @@ const PostExpample: PostProps[] = [{
 
 export default function Post() {
 
+    const dispatch = useDispatch<AppDispatch>();
+    const { data: session } = useSession()
+    const [posts, setPosts] = useState<PostProps[]>()
+
+    const handleFetchPosts = async () => {
+        if (session?.user?.token) {
+            const result = await dispatch(fetchPosts({ token: session.user.token }));
+            if (result.type == 'posts/fetchPosts/fulfilled') {
+                setPosts(result.payload);
+            }
+        }
+    }
+
+    useEffect(() => {
+        handleFetchPosts();
+    }, [session])
+
 
     return (
         <div className='flex flex-col mx-auto px-4 py-8 h-1/2 w-full max-w-2xl'>
             {
-                PostExpample.map((post) => (
+                posts && posts.map((post) => (
                     <PostsCards key={post.id} post={post} />
                 ))
             }
--- src/app/types/Api.d.ts
+++ src/app/types/Api.d.ts
@@ -9,7 +9,7 @@ declare global {
     }
     type FetchPayload = {
         token: string,
-        params: Params
+        params?: Params
     }
 }
 
