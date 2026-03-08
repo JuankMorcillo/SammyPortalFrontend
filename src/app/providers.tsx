6aaf400a13c4628764a6a9911bd63e4f6a77630c
@juankmorcillo2 <juankmorcillo2@gmail.com>
2026-03-08 09:37:49 -0500

    feature se crea componente generico toast; se agrega a toda la pagina

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,11 +5,13 @@ import React, { ReactNode } from 'react'
 import Header from './components/ui/header'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import Toast from './components/ui/toast'
 
 export default function Providers({ children }: { children: ReactNode }) {
     return (
         <Provider store={store}>
             <SessionProvider>
+                <Toast />
                 <Header />
                 {children}
             </SessionProvider>
